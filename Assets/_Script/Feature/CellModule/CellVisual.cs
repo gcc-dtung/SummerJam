3be5f44fad40fad61885fd097239661e78fe0223
dtun.dev <ductung.dev@gmail.com>
2026-07-13 23:16:26 +0700

    Feat : Add ChangeViewBackground Slot When Hover + Refactor : Folder

---
+++ Assets/_Script/Feature/CellModule/CellVisual.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+using PrimeTween;
+
+public class CellVisual : MonoBehaviour
+{
+    [SerializeField] private CellEventHandler eventHandler;
+    [SerializeField] private float changeViewDuration;
+    [SerializeField] private float changeViewScale;
+    [SerializeField] private SpriteRenderer hoverSprite;
+    [SerializeField] private SpriteRenderer backgroundSprite;
+    [SerializeField] private Color backgroundColorWhenChange;
+    
+    private Tween scaleTween;
+    private float baseScale;
+    private int baseOrderInLayer;
+    private Color baseColor;
+    
+    private void Start()
+    {
+        baseScale = hoverSprite.transform.localScale.x;
+        baseOrderInLayer = hoverSprite.sortingOrder;
+        baseColor = backgroundSprite.color;
+    }
+
+    private void OnEnable()
+    {
+        eventHandler.OnSelected += ChangeVisualOnSelected;
+        eventHandler.OnDeselected += ChangeVisualOnDeselected;
+    }
+
+    private void OnDisable()
+    {
+        eventHandler.OnSelected -= ChangeVisualOnSelected;
+        eventHandler.OnDeselected -= ChangeVisualOnDeselected;
+    }
+
+    private void ChangeVisualHover(float viewScale, int orderInLayer)
+    {
+        if (scaleTween.isAlive)
+            scaleTween.Stop();
+        if (hoverSprite.transform.localScale == Vector3.one * viewScale) return;
+        hoverSprite.sortingOrder = orderInLayer;
+        scaleTween = Tween.Scale(hoverSprite.transform, viewScale, changeViewDuration);
+    }
+
+    private void ChangeVisualBackGround(Color c)
+    {
+        if (backgroundSprite.color == c) return;
+        Color newColor = c;
+        newColor.a = 1;
+        backgroundSprite.color = newColor;
+    }
+
+    public void ChangeVisualOnSelected()
+    {
+        ChangeVisualHover(changeViewScale, Constaints.MAX_SORTING_LAYER);
+        ChangeVisualBackGround(backgroundColorWhenChange);
+    }
+
+    public void ChangeVisualOnDeselected()
+    {
+        ChangeVisualHover(baseScale, baseOrderInLayer);
+        ChangeVisualBackGround(baseColor);
+    }
+}

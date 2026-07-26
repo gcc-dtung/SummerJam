3be7a022eb7a0f11da126b628e8e47031f6db783
dtun.dev <ductung.dev@gmail.com>
2026-07-26 15:37:00 +0700

    feat : coin va gem

---
+++ Assets/_Script/Feature/Economy/EconomyUpdate.cs
@@ -0,0 +1,38 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class EconomyUpdate : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI[] cointText;
+    [SerializeField] private TextMeshProUGUI[] gemText;
+
+    private void OnEnable()
+    {
+        EconomyManager.Instance.OnGoldChange += UpdateCoin;
+        EconomyManager.Instance.OnGemChange += UpdateGem;
+    }
+
+    private void OnDisable()
+    {
+        EconomyManager.Instance.OnGoldChange -= UpdateCoin;
+        EconomyManager.Instance.OnGemChange -= UpdateGem;
+    }
+
+    private void UpdateCoin(int coin)
+    {
+        for(int i = 0; i < cointText.Length; i++)
+        {
+            cointText[i].text = coin.ToString();
+        }
+    }
+
+    private void UpdateGem(int gem)
+    {
+        for (int i = 0; i < gemText.Length; i++)
+        {
+            gemText[i].text = gem.ToString();
+        }
+    }
+
+}
